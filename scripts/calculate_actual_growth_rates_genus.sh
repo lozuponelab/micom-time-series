a55e74c3a310ac3c8b3cb7638943040515cb1a6c
Laurie Lyon <laurie.marie.lyon@gmail.com>
2026-06-24 11:40:38 -0600

    Delete scripts/calculate_actual_growth_rates_genus.sh

--- scripts/calculate_actual_growth_rates_genus.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python3 ./calculate_actual_growth_rates_genus.py \
-    --feature_tables_dir ../data/qiime_outputs/ \
-    --taxonomy_dir ../data/qiime_outputs \
-    --output_dir ../data/actual_growth_rates_genus_percent/
